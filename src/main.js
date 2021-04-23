93a741ff0564a41b01ef5dacb33481b93a13c553
Angel Mei <angelmei@Angels-MacBook-Air.local>
2021-04-22 22:01:26 -0700

    added slider

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import {InputNumber, DatePicker, ColorPicker} from 'element-ui';
+import {InputNumber, DatePicker, ColorPicker, Slider} from 'element-ui';
 
 Vue.config.productionTip = false
 Vue.use(InputNumber);
 Vue.use(DatePicker);
 Vue.use(ColorPicker);
+Vue.use(Slider);
 
 new Vue({
   render: h => h(App),
